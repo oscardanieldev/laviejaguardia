5c90487d8d1c77ea6a4284a6e8390aa5d0bb904a
Oscar <oscar.solares@networkshop.mx>
2020-11-04 11:20:13 -0600

    Galeria final

--- src/componentes/Galeria.jsx
+++ src/componentes/Galeria.jsx
@@ -1,47 +1,156 @@
 import React, { Fragment } from 'react';
+import Gallery from 'react-photo-gallery';
 import Piedepagina from './Piedepagina';
 const Galeria = () => {
 
+    const photos = [
+        {
+          src: 'imagenes/g25.png',
+          width: 2,
+          height: 3,
+          className: 'borde-galeria'
+        },
+        {
+          src: 'imagenes/g24.png',
+          width: 5,
+          height: 3,
+          className: 'borde-galeria'
+        },
+        {
+            src: 'imagenes/g23.png',
+            width: 2,
+            height: 3,
+            className: 'borde-galeria'
+        },
+        {
+            src: 'imagenes/g22.png',
+            width: 3,
+            height: 2,
+            className: 'borde-galeria'
+        },
+        {
+            src: 'imagenes/g21.png',
+            width: 2,
+            height: 3,
+            className: 'borde-galeria'
+        },
+        {
+            src: 'imagenes/g20.png',
+            width: 4,
+            height: 3,
+            className: 'borde-galeria'
+        },
+        {
+            src: 'imagenes/g18.png',
+            width: 3,
+            height: 3,
+            className: 'borde-galeria'
+        },
+        {
+            src: 'imagenes/g17.png',
+            width: 3,
+            height: 3,
+            className: 'borde-galeria'
+        },
+        {
+            src: 'imagenes/g16.png',
+            width: 2,
+            height: 1,
+            className: 'borde-galeria'
+        },
+        {
+            src: 'imagenes/g15.png',
+            width: 1,
+            height: 1,
+            className: 'borde-galeria'
+        },
+        {
+            src: 'imagenes/g14.png',
+            width: 1,
+            height: 1,
+            className: 'borde-galeria'
+        },
+        {
+            src: 'imagenes/g13.png',
+            width: 2,
+            height: 1,
+            className: 'borde-galeria'
+        },
+        {
+            src: 'imagenes/g12.png',
+            width: 3,
+            height: 2,
+            className: 'borde-galeria'
+        },
+        {
+            src: 'imagenes/g11.png',
+            width: 1,
+            height: 2,
+            className: 'borde-galeria'
+        },
+        {
+            src: 'imagenes/g10.png',
+            width: 3,
+            height: 2,
+            className: 'borde-galeria'
+        },        {
+            src: 'imagenes/g9.png',
+            width: 5,
+            height: 3,
+            className: 'borde-galeria'
+        },        {
+            src: 'imagenes/g8.png',
+            width: 3,
+            height: 2,
+            className: 'borde-galeria'
+        },        {
+            src: 'imagenes/g7.png',
+            width: 2,
+            height: 3,
+            className: 'borde-galeria'
+        },        {
+            src: 'imagenes/g6.png',
+            width: 2,
+            height: 2,
+            className: 'borde-galeria'
+        },        {
+            src: 'imagenes/g5.png',
+            width: 2,
+            height: 2,
+            className: 'borde-galeria'
+        },        {
+            src: 'imagenes/g4.png',
+            width: 3,
+            height: 2 ,
+            className: 'borde-galeria'
+        },        {
+            src: 'imagenes/g3.png',
+            width: 2,
+            height: 2,
+            className: 'borde-galeria'
+        },        {
+            src: 'imagenes/g2.png',
+            width: 2,
+            height: 3,
+            className: 'borde-galeria'
+        },        {
+            src: 'imagenes/g1.png',
+            width: .25,
+            height: .5,
+            className: 'borde-galeria'
+        },
+      ]
   
     return ( 
         <Fragment>
             <div className="bg-galeria">
                 <div className="container">
-                    <div className="row">
-                        <h2 className="col s12 m-salon center-align color-crema">
-                            Galería
-                        </h2>
-                        <div className="row">
-                            <div className="carousel ">
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g25.png" alt="imagen 1"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g2.png" alt="imagen 2"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g3.png" alt="imagen 3"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g4.png" alt="imagen 4"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g5.png" alt="imagen 5"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g6.png" alt="imagen 6"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g7.png" alt="imagen 7"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g8.png" alt="imagen 8"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g9.png" alt="imagen 9"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g10.png" alt="imagen 10"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g11.png" alt="imagen 11"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g12.png" alt="imagen 12"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g13.png" alt="imagen 13"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g14.png" alt="imagen 14"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g15.png" alt="imagen 15"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g16.png" alt="imagen 16"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g17.png" alt="imagen 17"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g18.png" alt="imagen 18"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g19.png" alt="imagen 19"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g20.png" alt="imagen 20"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g21.png" alt="imagen 21"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g22.png" alt="imagen 22"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g23.png" alt="imagen 23"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g24.png" alt="imagen 24"/></a>
-                                <a className="carousel-item" href="#one!"><img className="responsive-img materialboxed"  src="imagenes/g21.png" alt="imagen 25"/></a>
-                                
-
-                            </div>
-                                
+                    <div className="row ">
+                        <div className="col s12 sucursal-mt">
+                            <h3 className="color-crema center-align">Galería</h3>
+                        </div>
+                        <div className="col s12">
+                            <Gallery photos={photos} />
                         </div>
                     </div>
                 </div>
